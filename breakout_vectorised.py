fbc5571fc2210c8da9c65bdd91c1c05536ed6309
justin.mak <justin.mak@int.oceaninfinity.com>
2022-08-01 10:00:26 +0100

    Finish tutorial

--- breakout_vectorised.py
+++ breakout_vectorised.py
@@ -1,4 +1,4 @@
-"""Render vectorised environments; display screenshots of the games in a tiled fashion."""
+"""Render vectorised environments."""
 
 import time
 
@@ -6,8 +6,13 @@ import gym
 
 from stable_baselines3.common.vec_env.subproc_vec_env import SubprocVecEnv
 
+
 def main(num_envs=3):
-    envs = [lambda: gym.make('ALE/Breakout-v5', render_mode="human") for _ in range(num_envs)]
+    """Display games in popouts."""
+    envs = [
+        lambda: gym.make("ALE/Breakout-v5", render_mode="human")
+        for _ in range(num_envs)
+    ]
 
     # Vectorise envs
     envs = SubprocVecEnv(envs)
@@ -22,12 +27,13 @@ def main(num_envs=3):
     ob_ind = 0
     print(f"Shape of env {ob_ind}:", init_obs[ob_ind].shape)
 
-    for i in range(1000):
+    for _ in range(1000):
         actions = [envs.action_space.sample() for j in range(num_envs)]
         envs.step(actions)
         time.sleep(0.001)
 
     envs.close()
+
 
 if __name__ == "__main__":
     main()
